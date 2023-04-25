cc2aa2b0407b6023e4a48218048f52c28d9f065e
dipaliwaghmode024@gmail.com <dipaliwaghmode024@gmail.com>
2023-04-25 10:54:01 +0000

    code

--- models/DIM_ACCOUNT1.sql
+++ models/DIM_ACCOUNT1.sql
@@ -8,7 +8,7 @@ with events as(
     select * from  {{source('PC_DBT_DB', 'ACCOUNT')}}
     {% if is_incremental() %}
     {{ log("This is an incremental run", info=True) }}
-    where DBT_UPDATED_AT >= (select max(DBT_UPDATED_AT) from {{this}} )
+    where CREATED_AT >= (select max(CREATED_AT) from {{this}} )
     {% endif %}
 )
 select * from events
